32e9668eed727ee5628010901cc4e8f16f90ea27
jicruz96 <accounts@jicruz.com>
2020-05-12 15:47:27 +0000

    Push updated header to repo

--- holberton.h
+++ holberton.h
@@ -6,43 +6,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
+#include <stdbool.h>
 
 /**
- * struct f_str -	struct that pairs a char format specifier with its
- *					corresponding printing function
- * @c: format specifier
- * @f: function called
+ * struct num_s - struct that stores number formatting configurations
+ * @spec: format specifier
+ * @power: exp value associated with @c. determines function behavior
+ * @hex_char: assists in printing chars for hex bases. 0 for other bases
  */
-typedef struct f_str
+typedef struct num_s
 {
-	char c;
-	int (*f)(va_list);
-} f_str;
+	char spec;
+	int power;
+	int hex_char;
+} num_t;
 
 /**
- * struct f_num - struct that pairs an int format specifier with a base value
- * @c: format specifier
- * @exp: exp value associated with @c. determines function behavior
- * @hex: assists in printing characters for hexadecimal base. 0 for other bases
+ * struct config_s - struct that stores print format configuration settings
+ * @spec: format specifier code
+ * @longint: if 1, print arg as a long int
+ * @shortint: if 1, print arg as a short int
+ * @minus: if 1, print arg right-aligned
+ * @plus: if 1, print the '+' sign before a number arg
+ * @space: if 1, print placeholder chars with spaces
+ * @zero: if 1, print placeholder chars with zeroes
+ * @hash: if 1, print non-base 10 digits with prefixes
+ * @width: minimum number of chars to print
+ * @precision: maximum number of chars to print
+ * @str: if the argument is a string, it is stored here
+ * @arg: if the argument is anything else, it is stored here
  */
-typedef struct f_num
+typedef struct config_s
 {
-	char c;
-	int exp;
-	int hex;
-} f_num;
+	char spec;
+	int longint;
+	int shortint;
+	int minus;
+	int plus;
+	int space;
+	int zero;
+	int hash;
+	int width;
+	int precision;
+	char *str;
+	unsigned long int arg;
+} config_t;
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int getprinter(const char **, va_list);
-int p_c(va_list);
-int p_s(va_list);
-int p_r(va_list);
-int p_R(va_list);
-int p_S(va_list);
-int p_p(va_list);
-int p_num(f_num, unsigned long int, int);
-int p_int(char, unsigned int);
-int p_long(char, unsigned long int);
+void getprinter(const char **, char **, va_list);
+config_t format_config(const char ***c);
+num_t *format_numconfig(char c);
+char *p_c(config_t settings, va_list list);
+char *p_s(config_t);
+char *p_num(config_t);
+char *p_uidc(config_t config);
+size_t _strlen(char *s);
 
 #endif
